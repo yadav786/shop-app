da7d57dec13f200aa3198921e4d5ed6c5ff74e2f
Pankaj Yadav <pankaj.azm2@gmail.com>
2020-09-15 03:28:34 +0530

    push notification added

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -20,7 +20,8 @@ const CartScreen = props => {
         productTitle: state.cart.items[key].productTitle,
         productPrice: state.cart.items[key].productPrice,
         quantity: state.cart.items[key].quantity,
-        sum: state.cart.items[key].sum
+        sum: state.cart.items[key].sum,
+        productPushToken: state.cart.items[key].pushToken
       });
     }
     return transformedCartItems.sort((a, b) =>
